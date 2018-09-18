8168598a7aded437259cc6f75f72bbc707ceb3c4
Sharmina Akhtar <sakht318@gmail.com>
2018-09-18 21:50:15 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
   def create
-    gitserv = GithubService.new
+    gits = GithubService.new
     session[:token] = gitserv.authenticate!(ENV["GITHUB_CLIENT"], ENV["GITHUB_SECRET"], params[:code])
-    session[:username] = gitserv.get_username
+    session[:username] = gits.get_username
 
     redirect_to '/'
   end
